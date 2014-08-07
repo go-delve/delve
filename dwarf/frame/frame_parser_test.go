631c8d902db87adc573ea86d3ce131effd5806d9
Derek Parker <parkerderek86@gmail.com>
2014-08-07 11:04:07 -0500

    Remove dead test

--- dwarf/frame/frame_parser_test.go
+++ dwarf/frame/frame_parser_test.go
@@ -8,39 +8,6 @@ import (
 	"github.com/derekparker/dbg/dwarf/frame"
 )
 
-// func TestParse(t *testing.T) {
-// 	var (
-// 		data = dwarfhelper.GrabDebugFrameSection("../../_fixtures/testprog", t)
-// 		fe   = frame.Parse(data)[0]
-// 		ce   = fe.CIE
-// 	)
-
-// 	if ce.Length != 16 {
-// 		t.Error("Length was not parsed correctly, got ", ce.Length)
-// 	}
-
-// 	if ce.Version != 0x3 {
-// 		t.Fatalf("Version was not parsed correctly expected %#v got %#v", 0x3, ce.Version)
-// 	}
-
-// 	if ce.Augmentation != "" {
-// 		t.Fatal("Augmentation was not parsed correctly")
-// 	}
-
-// 	if ce.CodeAlignmentFactor != 0x1 {
-// 		t.Fatal("Code Alignment Factor was not parsed correctly")
-// 	}
-
-// 	if ce.DataAlignmentFactor != -4 {
-// 		t.Fatalf("Data Alignment Factor was not parsed correctly got %#v", ce.DataAlignmentFactor)
-// 	}
-
-// 	if fe.Length != 32 {
-// 		t.Fatal("Length was not parsed correctly, got ", fe.Length)
-// 	}
-
-// }
-
 func BenchmarkParse(b *testing.B) {
 	defer profile.Start(profile.CPUProfile).Stop()
 	data := dwarfhelper.GrabDebugFrameSection("../../_fixtures/testprog", nil)
