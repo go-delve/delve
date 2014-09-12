a74e8bf14a2426b7263b7dada1dc1cc3f35af15d
Derek Parker <parkerderek86@gmail.com>
2014-09-12 15:25:00 -0500

    Recompile test prog for main_test

--- main_test.go
+++ main_test.go
@@ -33,9 +33,14 @@ func startDebugger(t *testing.T, pid int) *os.Process {
 }
 
 func startTestProg(t *testing.T, proc string) *os.Process {
+	err := exec.Command("go", "build", "-gcflags=-N -l", "-o", proc, proc+".go").Run()
+	if err != nil {
+		t.Fatal("Could not compile", proc, err)
+	}
+	defer os.Remove(proc)
 	cmd := exec.Command(proc)
 
-	err := cmd.Start()
+	err = cmd.Start()
 	if err != nil {
 		t.Fatal(err)
 	}
