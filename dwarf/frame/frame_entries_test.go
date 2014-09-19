04c5f785f96739473ff0223c1a85e18a4236b368
Derek Parker <parkerderek86@gmail.com>
2014-09-19 16:44:20 -0500

    Remove unused test helper funcs

--- dwarf/frame/frame_entries_test.go
+++ dwarf/frame/frame_entries_test.go
@@ -1,71 +1,10 @@
 package frame
 
 import (
-	"debug/elf"
-	"debug/gosym"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"testing"
 )
-
-func gosymData(testfile string, t testing.TB) *gosym.Table {
-	f, err := os.Open(testfile)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	e, err := elf.NewFile(f)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return parseGoSym(t, e)
-}
-
-func grabDebugFrameSection(fp string, t testing.TB) []byte {
-	p, err := filepath.Abs(fp)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	f, err := os.Open(p)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	ef, err := elf.NewFile(f)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	data, err := ef.Section(".debug_frame").Data()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return data
-}
-
-func parseGoSym(t testing.TB, exe *elf.File) *gosym.Table {
-	symdat, err := exe.Section(".gosymtab").Data()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	pclndat, err := exe.Section(".gopclntab").Data()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	pcln := gosym.NewLineTable(pclndat, exe.Section(".text").Addr)
-	tab, err := gosym.NewTable(symdat, pcln)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	return tab
-}
 
 func TestFDEForPC(t *testing.T) {
 	fde1 := &FrameDescriptionEntry{AddressRange: &addrange{begin: 100, end: 200}}
