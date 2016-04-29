41fd7d7901b0e5f1407cfb31419c0fb8a2249b83
Cameron Taggart <cameron@tachyus.com>
2016-04-29 12:03:12 -0700

    attempt to chdir on darwin via @yuntan

--- proc/proc_darwin.go
+++ proc/proc_darwin.go
@@ -64,6 +64,7 @@ func Launch(cmd []string, wd string) (*Process, error) {
 	var pid int
 	dbp.execPtraceFunc(func() {
 		ret := C.fork_exec(argv0, &argvSlice[0], C.int(len(argvSlice)),
+			C.CString(wd),
 			&dbp.os.task, &dbp.os.portSet, &dbp.os.exceptionPort,
 			&dbp.os.notificationPort)
 		pid = int(ret)
