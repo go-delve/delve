343142b20e39378dff879750945a2815421c7c6e
aarzilli <alessandro.arzilli@gmail.com>
2016-07-14 09:43:39 +0200

    proc: bugfix: Next and normal calls to deferred function
    When a deferred function is called directly Next and StepOut should not
    step into it.
    
    Fixes #582

--- proc/proc_test.go
+++ proc/proc_test.go
@@ -1895,3 +1895,30 @@ func Test1Issue573(t *testing.T) {
 		assertNoError(p.Step(), t, "Step() #3") // Third step ought to be possible; program ought not have exited.
 	})
 }
+
+func TestNextDeferReturnAndDirectCall(t *testing.T) {
+	// Next should not step into a deferred function if it is called
+	// directly, only if it is called through a panic or a deferreturn.
+	// Here we test the case where the function is called by a deferreturn
+	testnext("defercall", []nextTest{
+		{9, 10},
+		{10, 11},
+		{11, 12},
+		{12, 13},
+		{13, 5},
+		{5, 6},
+		{6, 7},
+		{7, 13},
+		{13, 28}}, "main.callAndDeferReturn", t)
+}
+
+func TestNextPanicAndDirectCall(t *testing.T) {
+	// Next should not step into a deferred function if it is called
+	// directly, only if it is called through a panic or a deferreturn.
+	// Here we test the case where the function is called by a panic
+	testnext("defercall", []nextTest{
+		{15, 16},
+		{16, 17},
+		{17, 18},
+		{18, 5}}, "main.callAndPanic2", t)
+}
