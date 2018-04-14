303cba43da3343fa88473e9f7fd1efeaab24bfd5
aarzilli <alessandro.arzilli@gmail.com>
2018-04-14 14:57:35 +0200

    proc: change memCache to delay reading
    Change memCache so that the preloaded memory is not read immediately
    but only after the actual read to the preloaded range.
    
    This allows us to request caching the entire stack frame every time we
    create an eval scope and no unnecessary reads will be made even if the
    user is just trying to evaluate a global variable.

--- pkg/proc/proc_general_test.go
+++ pkg/proc/proc_general_test.go
@@ -7,7 +7,7 @@ import (
 func TestIssue554(t *testing.T) {
 	// unsigned integer overflow in proc.(*memCache).contains was
 	// causing it to always return true for address 0xffffffffffffffff
-	mem := memCache{0x20, make([]byte, 100), nil}
+	mem := memCache{true, 0x20, make([]byte, 100), nil}
 	if mem.contains(0xffffffffffffffff, 40) {
 		t.Fatalf("should be false")
 	}
