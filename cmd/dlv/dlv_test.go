ac844e64d5713813fc311a511e374bf2c0a8d567
aarzilli <alessandro.arzilli@gmail.com>
2018-11-20 16:13:09 +0100

    tests: fix typecheckrpc.go and convert it to a test
    We forgot to run typecheckrpc.go periodically and it didn't work
    anymore and there were some minor errors in service/rpc2/client.go.
    
    Rewrite typecheckrpc.go using go/packages, so that it works with go1.11
    and go.mod, and fix the issues in client.go

--- cmd/dlv/dlv_test.go
+++ cmd/dlv/dlv_test.go
@@ -5,11 +5,15 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
+	"go/ast"
+	"go/token"
+	"go/types"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -19,6 +23,8 @@ import (
 	"github.com/go-delve/delve/pkg/terminal"
 	"github.com/go-delve/delve/service/rpc2"
 	"github.com/spf13/cobra/doc"
+
+	"golang.org/x/tools/go/packages"
 )
 
 var testBackend string
@@ -271,5 +277,186 @@ func TestExitInInit(t *testing.T) {
 	// prompt once if the init file didn't call exit successfully.
 	if strings.Contains(string(out), "(dlv)") {
 		t.Fatal("init did not cause dlv to exit")
+	}
+}
+
+func getMethods(pkg *types.Package, typename string) map[string]*types.Func {
+	r := make(map[string]*types.Func)
+	mset := types.NewMethodSet(types.NewPointer(pkg.Scope().Lookup(typename).Type()))
+	for i := 0; i < mset.Len(); i++ {
+		fn := mset.At(i).Obj().(*types.Func)
+		r[fn.Name()] = fn
+	}
+	return r
+}
+
+func publicMethodOf(decl ast.Decl, receiver string) *ast.FuncDecl {
+	fndecl, isfunc := decl.(*ast.FuncDecl)
+	if !isfunc {
+		return nil
+	}
+	if fndecl.Name.Name[0] >= 'a' && fndecl.Name.Name[0] <= 'z' {
+		return nil
+	}
+	if fndecl.Recv == nil || len(fndecl.Recv.List) != 1 {
+		return nil
+	}
+	starexpr, isstar := fndecl.Recv.List[0].Type.(*ast.StarExpr)
+	if !isstar {
+		return nil
+	}
+	identexpr, isident := starexpr.X.(*ast.Ident)
+	if !isident || identexpr.Name != receiver {
+		return nil
+	}
+	if fndecl.Body == nil {
+		return nil
+	}
+	return fndecl
+}
+
+func findCallCall(fndecl *ast.FuncDecl) *ast.CallExpr {
+	for _, stmt := range fndecl.Body.List {
+		var x ast.Expr = nil
+
+		switch s := stmt.(type) {
+		case *ast.AssignStmt:
+			if len(s.Rhs) == 1 {
+				x = s.Rhs[0]
+			}
+		case *ast.ReturnStmt:
+			if len(s.Results) == 1 {
+				x = s.Results[0]
+			}
+		case *ast.ExprStmt:
+			x = s.X
+		}
+
+		callx, iscall := x.(*ast.CallExpr)
+		if !iscall {
+			continue
+		}
+		fun, issel := callx.Fun.(*ast.SelectorExpr)
+		if !issel || fun.Sel.Name != "call" {
+			continue
+		}
+		return callx
+	}
+	return nil
+}
+
+func qf(*types.Package) string {
+	return ""
+}
+
+func TestTypecheckRPC(t *testing.T) {
+	fset := &token.FileSet{}
+	cfg := &packages.Config{
+		Mode: packages.LoadSyntax,
+		Fset: fset,
+	}
+	pkgs, err := packages.Load(cfg, "github.com/go-delve/delve/service/rpc2")
+	if err != nil {
+		t.Fatal(err)
+	}
+	var clientAst *ast.File
+	var serverMethods map[string]*types.Func
+	var info *types.Info
+	packages.Visit(pkgs, func(pkg *packages.Package) bool {
+		if pkg.PkgPath != "github.com/go-delve/delve/service/rpc2" {
+			return true
+		}
+		t.Logf("package found: %v", pkg.PkgPath)
+		serverMethods = getMethods(pkg.Types, "RPCServer")
+		info = pkg.TypesInfo
+		for i := range pkg.Syntax {
+			t.Logf("file %q", pkg.CompiledGoFiles[i])
+			if strings.HasSuffix(pkg.CompiledGoFiles[i], string(os.PathSeparator)+"client.go") {
+				clientAst = pkg.Syntax[i]
+				break
+			}
+		}
+		return true
+	}, nil)
+
+	errcount := 0
+
+	for _, decl := range clientAst.Decls {
+		fndecl := publicMethodOf(decl, "RPCClient")
+		if fndecl == nil {
+			continue
+		}
+
+		switch fndecl.Name.Name {
+		case "Continue", "Rewind":
+			// wrappers over continueDir
+			continue
+		case "SetReturnValuesLoadConfig", "Disconnect":
+			// support functions
+			continue
+		}
+
+		if fndecl.Name.Name == "Continue" || fndecl.Name.Name == "Rewind" {
+			// using continueDir
+			continue
+		}
+
+		callx := findCallCall(fndecl)
+
+		if callx == nil {
+			t.Errorf("%s: could not find RPC call", fset.Position(fndecl.Pos()))
+			errcount++
+			continue
+		}
+
+		if len(callx.Args) != 3 {
+			t.Errorf("%s: wrong number of arguments for RPC call", fset.Position(callx.Pos()))
+			errcount++
+			continue
+		}
+
+		arg0, arg0islit := callx.Args[0].(*ast.BasicLit)
+		arg1 := callx.Args[1]
+		arg2 := callx.Args[2]
+		if !arg0islit || arg0.Kind != token.STRING {
+			continue
+		}
+		name, _ := strconv.Unquote(arg0.Value)
+		serverMethod := serverMethods[name]
+		if serverMethod == nil {
+			t.Errorf("%s: could not find RPC method %q", fset.Position(callx.Pos()), name)
+			errcount++
+			continue
+		}
+
+		params := serverMethod.Type().(*types.Signature).Params()
+
+		if a, e := info.TypeOf(arg1), params.At(0).Type(); !types.AssignableTo(a, e) {
+			t.Errorf("%s: wrong type of first argument %s, expected %s", fset.Position(callx.Pos()), types.TypeString(a, qf), types.TypeString(e, qf))
+			errcount++
+			continue
+		}
+
+		if !strings.HasSuffix(params.At(1).Type().String(), "/service.RPCCallback") {
+			if a, e := info.TypeOf(arg2), params.At(1).Type(); !types.AssignableTo(a, e) {
+				t.Errorf("%s: wrong type of second argument %s, expected %s", fset.Position(callx.Pos()), types.TypeString(a, qf), types.TypeString(e, qf))
+				errcount++
+				continue
+			}
+		}
+
+		if clit, ok := arg1.(*ast.CompositeLit); ok {
+			typ := params.At(0).Type()
+			st := typ.Underlying().(*types.Struct)
+			if len(clit.Elts) != st.NumFields() && types.TypeString(typ, qf) != "DebuggerCommand" {
+				t.Errorf("%s: wrong number of fields in first argument's literal %d, expected %d", fset.Position(callx.Pos()), len(clit.Elts), st.NumFields())
+				errcount++
+				continue
+			}
+		}
+	}
+
+	if errcount > 0 {
+		t.Errorf("%d errors", errcount)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,18 +2,19 @@ module github.com/go-delve/delve
 
 require (
 	github.com/cosiner/argv v0.0.0-20170225145430-13bacc38a0a5
-	github.com/cpuguy83/go-md2man v1.0.8
-	github.com/inconshreveable/mousetrap v1.0.0
+	github.com/cpuguy83/go-md2man v1.0.8 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/mattn/go-colorable v0.0.0-20170327083344-ded68f7a9561
 	github.com/mattn/go-isatty v0.0.3
 	github.com/peterh/liner v0.0.0-20170317030525-88609521dc4b
 	github.com/pkg/profile v0.0.0-20170413231811-06b906832ed0
-	github.com/russross/blackfriday v0.0.0-20180428102519-11635eb403ff
+	github.com/russross/blackfriday v0.0.0-20180428102519-11635eb403ff // indirect
 	github.com/sirupsen/logrus v0.0.0-20180523074243-ea8897e79973
 	github.com/spf13/cobra v0.0.0-20170417170307-b6cb39589372
-	github.com/spf13/pflag v0.0.0-20170417173400-9e4c21054fa1
+	github.com/spf13/pflag v0.0.0-20170417173400-9e4c21054fa1 // indirect
 	golang.org/x/arch v0.0.0-20171004143515-077ac972c2e4
-	golang.org/x/crypto v0.0.0-20180614174826-fd5f17ee7299
+	golang.org/x/crypto v0.0.0-20180614174826-fd5f17ee7299 // indirect
 	golang.org/x/sys v0.0.0-20180614134839-8883426083c0
+	golang.org/x/tools v0.0.0-20181120060634-fc4f04983f62
 	gopkg.in/yaml.v2 v2.0.0-20170407172122-cd8b52f8269e
 )
