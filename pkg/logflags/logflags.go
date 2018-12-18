23046c23b88863d8dad7a837f9bb05a158fc3a7f
aarzilli <alessandro.arzilli@gmail.com>
2018-12-18 18:30:07 +0100

    logflags: replace default formatter of logrus
    The default formatter of logrus emits logs in two different formats
    depending on whether or not the output is going to a terminal. The
    output format for non-terminals is indented to be machine readable, but
    we mostly read logs ourselves and the excessive quoting makes that
    format unreadable.
    When outputting to terminals it uses ANSI escape codes unconditionally,
    without checking whether the terminal it is connected to actually
    supports colors.
    
    This commit replaces the default formatter with a much simpler
    formatter that always uses a more readable format, doesn't use colors
    and places the key-value pairs at the beginning of the line (which is a
    better match for how we use them).

--- pkg/logflags/logflags.go
+++ pkg/logflags/logflags.go
@@ -1,10 +1,14 @@
 package logflags
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"log"
+	"sort"
 	"strings"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -19,6 +23,7 @@ var minidump = false
 
 func makeLogger(flag bool, fields logrus.Fields) *logrus.Entry {
 	logger := logrus.New().WithFields(fields)
+	logger.Logger.Formatter = &textFormatter{}
 	logger.Logger.Level = logrus.DebugLevel
 	if !flag {
 		logger.Logger.Level = logrus.PanicLevel
@@ -122,4 +127,63 @@ func Setup(logFlag bool, logstr string) error {
 		}
 	}
 	return nil
+}
+
+// textFormatter is a simplified version of logrus.TextFormatter that
+// doesn't make logs unreadable when they are output to a text file or to a
+// terminal that doesn't support colors.
+type textFormatter struct {
+}
+
+func (f *textFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	var b *bytes.Buffer
+	if entry.Buffer != nil {
+		b = entry.Buffer
+	} else {
+		b = &bytes.Buffer{}
+	}
+
+	keys := make([]string, 0, len(entry.Data))
+	for k := range entry.Data {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	b.WriteString(entry.Time.Format(time.RFC3339))
+	b.WriteByte(' ')
+	b.WriteString(entry.Level.String())
+	b.WriteByte(' ')
+	for i, key := range keys {
+		b.WriteString(key)
+		b.WriteByte('=')
+		stringVal, ok := entry.Data[key].(string)
+		if !ok {
+			stringVal = fmt.Sprint(entry.Data[key])
+		}
+		if f.needsQuoting(stringVal) {
+			fmt.Fprintf(b, "%q", stringVal)
+		} else {
+			b.WriteString(stringVal)
+		}
+		if i != len(keys)-1 {
+			b.WriteByte(',')
+		} else {
+			b.WriteByte(' ')
+		}
+	}
+	b.WriteString(entry.Message)
+	b.WriteByte('\n')
+	return b.Bytes(), nil
+}
+
+func (f *textFormatter) needsQuoting(text string) bool {
+	for _, ch := range text {
+		if !((ch >= 'a' && ch <= 'z') ||
+			(ch >= 'A' && ch <= 'Z') ||
+			(ch >= '0' && ch <= '9') ||
+			ch == '-' || ch == '.' || ch == '_' || ch == '/' || ch == '@' || ch == '^' || ch == '+') {
+			return true
+		}
+	}
+	return false
 }
