10cc8ab37e15365e0c3bd3215005b28529e1a86a
Brian de Alwis <bsd@mt.ca>
2019-07-16 23:51:55 -0400

    terminal: prompt to kill remote if process exited

--- pkg/terminal/terminal_test.go
+++ pkg/terminal/terminal_test.go
@@ -1,6 +1,8 @@
 package terminal
 
 import (
+	"errors"
+	"net/rpc"
 	"runtime"
 	"testing"
 
@@ -82,6 +84,24 @@ func TestSubstitutePath(t *testing.T) {
 		res := New(nil, &config.Config{SubstitutePath: subRules}).substitutePath(c.path)
 		if c.res != res {
 			t.Errorf("terminal.SubstitutePath(%q) => %q, want %q", c.path, res, c.res)
+		}
+	}
+}
+
+func TestIsErrProcessExited(t *testing.T) {
+	tests := []struct {
+		name   string
+		err    error
+		result bool
+	}{
+		{"empty error", errors.New(""), false},
+		{"non-ServerError", errors.New("Process 33122 has exited with status 0"), false},
+		{"ServerError with zero status", rpc.ServerError("Process 33122 has exited with status 0"), true},
+		{"ServerError with non-zero status", rpc.ServerError("Process 2 has exited with status 25"), true},
+	}
+	for _, test := range tests {
+		if isErrProcessExited(test.err) != test.result {
+			t.Error(test.name)
 		}
 	}
 }
