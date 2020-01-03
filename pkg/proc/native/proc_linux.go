bf3a343ed6be7d62d85041ac84d7d99fb0dd9c7c
Derek Parker <parkerderek86@gmail.com>
2020-01-03 11:40:16 -0800

    pkg/proc: Fix linux

--- pkg/proc/native/proc_linux.go
+++ pkg/proc/native/proc_linux.go
@@ -209,9 +209,6 @@ func (dbp *Process) addThread(tid int, attach bool) (*Thread, error) {
 		dbp: dbp,
 		os:  new(OSSpecificDetails),
 	}
-	if dbp.currentThread == nil {
-		dbp.SwitchThread(tid)
-	}
 	return dbp.threads[tid], nil
 }
 
