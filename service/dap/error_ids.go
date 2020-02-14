147ba1befe76c227fd0a4d94502e423f9f126070
Polina Sokolova <polinasok@users.noreply.github.com>
2020-02-14 01:48:42 -0800

    Address review comments

---
+++ service/dap/error_ids.go
@@ -0,0 +1,15 @@
+package dap
+
+// Unique identifiers for messages returned for errors from requests.
+const (
+	UnsupportedCommand int = 9999
+
+	// The values below come from the vscode-go debug adaptor.
+	// Although the spec says they should be unique, the adaptor
+	// reuses 3000 for launch, attach and program exit failures.
+	// TODO(polina): confirm if the extension expects specific ids
+	// for specific cases, and we must match the existing adaptor
+	// or if these codes can evolve.
+	FailedToContinue = 3000
+	// Add more codes as we support more requests
+)
