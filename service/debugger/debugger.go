aa7e4f5fb277e4530fb70a6e389cfb8568ebfce5
aarzilli <alessandro.arzilli@gmail.com>
2020-02-25 12:20:52 +0100

    debugger: CreateBreakpoint should delete existing breakpoints
    Fixes a bug introduced by the logical breakpoint change, where creating
    the same breakpoint twice deletes the breakpoint.

--- service/debugger/debugger.go
+++ service/debugger/debugger.go
@@ -486,6 +486,9 @@ func createLogicalBreakpoint(p proc.Process, addrs []uint64, requestedBp *api.Br
 		}
 	}
 	if err != nil {
+		if isBreakpointExistsErr(err) {
+			return nil, err
+		}
 		for _, bp := range bps {
 			if bp == nil {
 				continue
@@ -499,6 +502,11 @@ func createLogicalBreakpoint(p proc.Process, addrs []uint64, requestedBp *api.Br
 	}
 	createdBp := api.ConvertBreakpoints(bps)
 	return createdBp[0], nil // we created a single logical breakpoint, the slice here will always have len == 1
+}
+
+func isBreakpointExistsErr(err error) bool {
+	_, r := err.(proc.BreakpointExistsError)
+	return r
 }
 
 // AmendBreakpoint will update the breakpoint with the matching ID.
