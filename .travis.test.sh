0b69c9b3a1de367718b3d963b5b7651246529389
chainhelen <chainhelen@gmail.com>
2020-03-15 01:49:21 +0800

    Fix: go_version: command not found

--- .travis.test.sh
+++ .travis.test.sh
@@ -1,6 +1,6 @@
 if [ $TRAVIS_OS_NAME = "linux" ] && [ $go_32_version ]; then
   docker pull i386/ubuntu:bionic 
-  docker run -e "go_version=$go_32_version" -v $(pwd):/delve i386/ubuntu:bionic /bin/bash -c "cd delve && \
+  docker run -e go_version="$go_32_version" -v $(pwd):/delve i386/ubuntu:bionic /bin/bash -c "cd delve && \
   apt-get -y update && \
   apt-get -y install software-properties-common && \
   apt-get -y install git && \
