8e072ca1ad9251b32b9655d70267a1c3787709cd
chainhelen <chainhelen@gmail.com>
2020-03-15 02:41:52 +0800

    Add "docker help run"

--- .travis.test.sh
+++ .travis.test.sh
@@ -1,12 +1,13 @@
 if [ $TRAVIS_OS_NAME = "linux" ] && [ $go_32_version ]; then
   docker pull i386/ubuntu:bionic
-  docker run --env go_version=1.12 -v $(pwd):/delve i386/ubuntu:bionic /bin/bash -c "cd delve && \
+  docker help run
+  docker run --env goversion=1.12 -v $(pwd):/delve i386/ubuntu:bionic /bin/bash -c "cd delve && \
   apt-get -y update && \
   apt-get -y install software-properties-common && \
   apt-get -y install git && \
   add-apt-repository ppa:longsleep/golang-backports && \
-  apt-get -y install golang-$(go_version)-go && \
-  export PATH=$PATH:/usr/lib/go-$(go_version)/bin && \
+  apt-get -y install golang-$(goversion)-go && \
+  export PATH=$PATH:/usr/lib/go-$(goversion)/bin && \
   go version && \
   uname -a && \
   make test"
