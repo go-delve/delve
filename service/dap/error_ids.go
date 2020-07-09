c63ae072bcfcf418a6af772e906738b78ff7435b
polinasok <51177946+polinasok@users.noreply.github.com>
2020-07-09 14:39:04 -0700

    service/dap: resolve minor error code TODO (#2103)
    Co-authored-by: Polina Sokolova <polinasok@users.noreply.github.com>

--- service/dap/error_ids.go
+++ service/dap/error_ids.go
@@ -1,18 +1,17 @@
 package dap
 
 // Unique identifiers for messages returned for errors from requests.
+// These values are not mandated by DAP (other than the uniqueness
+// requirement), so each implementation is free to choose their own.
 const (
 	UnsupportedCommand int = 9999
 	InternalError      int = 8888
 	NotYetImplemented  int = 7777
 
-	// The values below come from the vscode-go debug adaptor.
-	// Although the spec says they should be unique, the adaptor
-	// reuses 3000 for launch, attach and program exit failures.
-	// TODO(polina): confirm if the extension expects specific ids
-	// for specific cases, and we must match the existing adaptor
-	// or if these codes can evolve.
-	FailedToContinue          = 3000
+	// Where applicable and for consistency only,
+	// values below are inspired the original vscode-go debug adaptor.
+	FailedToLaunch            = 3000
+	FailedtoAttach            = 3001
 	UnableToDisplayThreads    = 2003
 	UnableToProduceStackTrace = 2004
 	// Add more codes as we support more requests
