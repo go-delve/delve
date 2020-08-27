4c9723974fc6c1e0d21c931fb63ebf3965f8c9cf
chainhelen <chainhelen@gmail.com>
2020-08-27 11:59:44 +0800

    recover _fixtures/goroutinestackprog.go

--- _fixtures/goroutinestackprog.go
+++ _fixtures/goroutinestackprog.go
@@ -28,7 +28,7 @@ func main() {
 	for i := 0; i < N; i++ {
 		<-started
 	}
-	runtime.Gosched();time.Sleep(time.Duration(1) * time.Second);
+	runtime.Gosched()
 	stacktraceme()
 	for i := 0; i < N; i++ {
 		<-done
