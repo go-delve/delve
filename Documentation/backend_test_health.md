84328ed8703eb493072899d418ab04a4b37759e5
Alessandro Arzilli <alessandro.arzilli@gmail.com>
2020-09-24 17:10:20 +0200

    proc/tests: keep track of tests skipped due to backend problems (#2178)
    * proc/tests: keep track of tests skipped due to backend problems
    
    Mark tests skipped due to backend problems and add a script to keep
    track of them.
    
    * Travis-CI: add ignorechecksum option to chocolatey command
    
    Looks like a configuration problem on chocolatey's end.

---
+++ Documentation/backend_test_health.md
@@ -0,0 +1,19 @@
+Tests skipped by each supported backend:
+
+* 386 skipped = 1.4% (2/145)
+	* 2 broken - cgo stacktraces
+* arm64 skipped = 2.1% (3/145)
+	* 1 broken
+	* 1 broken - global variable symbolication
+	* 1 broken - reading defers
+* darwin/lldb skipped = 0.69% (1/145)
+	* 1 upstream issue
+* freebsd skipped = 7.6% (11/145)
+	* 11 broken
+* linux/386/pie skipped = 0.69% (1/145)
+	* 1 broken
+* pie skipped = 0.69% (1/145)
+	* 1 upstream issue - https://github.com/golang/go/issues/29322
+* windows skipped = 1.4% (2/145)
+	* 1 broken
+	* 1 upstream issue
---
+++ _scripts/gen-backend_test_health.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := parser.ParseFile(new(token.FileSet), "pkg/proc/proc_test.go", nil, 0)
+	if err != nil {
+		log.Fatalf("could not compile proc_test.go: %v", err)
+	}
+	ntests := 0
+	skipped := make(map[string]map[string]int)
+	ast.Inspect(f, func(node ast.Node) bool {
+		switch node := node.(type) {
+		case *ast.File:
+			return true
+		case *ast.FuncDecl:
+			if !strings.HasPrefix(node.Name.Name, "Test") {
+				return false
+			}
+			ntests++
+		stmtLoop:
+			for _, stmt := range node.Body.List {
+				expr, isexpr := stmt.(*ast.ExprStmt)
+				if !isexpr {
+					continue
+				}
+				call, ok := expr.X.(*ast.CallExpr)
+				if !ok {
+					return false
+				}
+				fun, ok := call.Fun.(*ast.Ident)
+				if !ok {
+					return false
+				}
+				switch fun.Name {
+				case "skipOn":
+					reason, conditions := skipOnArgs(call.Args)
+					if reason == "N/A" {
+						ntests--
+						break stmtLoop
+					}
+					if skipped[conditions] == nil {
+						skipped[conditions] = make(map[string]int)
+					}
+					skipped[conditions][reason]++
+				case "skipUnlessOn":
+					ntests--
+					break stmtLoop
+				}
+			}
+		}
+		return false
+	})
+	var fh io.WriteCloser
+	if len(os.Args) > 1 && os.Args[1] == "-" {
+		fh = os.Stdout
+	} else {
+		fh, err = os.Create("./Documentation/backend_test_health.md")
+		if err != nil {
+			log.Fatalf("could not create backend_test_health.md: %v", err)
+		}
+	}
+	fmt.Fprintf(fh, "Tests skipped by each supported backend:\n\n")
+	conds := []string{}
+	for cond := range skipped {
+		conds = append(conds, cond)
+	}
+	sort.Strings(conds)
+	for _, cond := range conds {
+		tot := 0
+		for _, v := range skipped[cond] {
+			tot += v
+		}
+		perc := float64(tot) / float64(ntests) * 100
+		fmt.Fprintf(fh, "* %s skipped = %0.02g%% (%d/%d)\n", cond, perc, tot, ntests)
+		reasons := []string{}
+		for reason := range skipped[cond] {
+			reasons = append(reasons, reason)
+		}
+		sort.Strings(reasons)
+		for _, reason := range reasons {
+			fmt.Fprintf(fh, "\t* %d %s\n", skipped[cond][reason], reason)
+		}
+	}
+	err = fh.Close()
+	if err != nil {
+		log.Fatalf("could not close output file: %v", err)
+	}
+}
+
+func skipOnArgs(args []ast.Expr) (reason string, conditions string) {
+	reason, _ = strconv.Unquote(args[1].(*ast.BasicLit).Value)
+	conds := []string{}
+	for _, arg := range args[2:] {
+		cond, _ := strconv.Unquote(arg.(*ast.BasicLit).Value)
+		conds = append(conds, cond)
+	}
+	conditions = strings.Join(conds, "/")
+	return reason, conditions
+}
