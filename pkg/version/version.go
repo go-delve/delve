f7542d69fe14ca3d43015a19e0094d1bddd9f90e
aarzilli <alessandro.arzilli@gmail.com>
2020-12-05 16:12:09 +0100

    all: bump version and release notes
    Thank you to: @polinasok, @aurkenb, @pohzipohzi, @chainhelen,
    @hitzhangjie, @eliben, @suzmue, @andreimatei, @aaronsky.

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -14,7 +14,7 @@ type Version struct {
 var (
 	// DelveVersion is the current version of Delve.
 	DelveVersion = Version{
-		Major: "1", Minor: "5", Patch: "0", Metadata: "",
+		Major: "1", Minor: "5", Patch: "1", Metadata: "",
 		Build: "$Id$",
 	}
 )
