4117d36ef80d6d4be190ded740044b7708c81bdb
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2021-01-12 22:34:34 +0100

    restore gdbserial constants for 386
    Otherwise compilation fails.

---
+++ pkg/proc/gdbserial/gdbserver_conn_386.go
@@ -0,0 +1,15 @@
+//+build 386
+
+package gdbserial
+
+const (
+	regnamePC     = "rip"
+	regnameCX     = "rcx"
+	regnameSP     = "rsp"
+	regnameDX     = "rdx"
+	regnameBP     = "rbp"
+	regnameFsBase = "fs_base"
+	regnameGsBase = "gs_base"
+
+	breakpointKind = 1
+)
