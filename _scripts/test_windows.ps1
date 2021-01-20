adc5546483e5c223dccf7f06f58a43cf40613582
aarzilli <alessandro.arzilli@gmail.com>
2021-01-20 17:10:37 +0100

    TeamCity: change test matrix and test scripts
    Changes test matrix to test:
    
    * 1.13 through 1.16 on linux/amd64
    * 1.15, 1.16 on windows/amd64
    * 1.15 on linux/386
    
    Also includes tests on go-tip for windows/amd64 and linux/amd64
    (currently disabled, but can be turned on in a PR).
    
    Changes test scripts to automatically select the latest patch version
    of Go.

--- _scripts/test_windows.ps1
+++ _scripts/test_windows.ps1
@@ -20,27 +20,50 @@ if (-Not(Test-Path "C:\procdump"))
     &7z x -oC:\procdump\ C:\procdump\procdump.zip > $null
 }
 
-# Install Go
-if ($version -eq "golatest")
-{
-    $version = Invoke-WebRequest -Uri https://golang.org/VERSION?m=text -UseBasicParsing | Select-Object -ExpandProperty Content
+$env:PATH += ";C:\procdump;C:\mingw64\bin"
+
+function GetGo($version) {
+    $env:GOROOT = "C:\go\$version"
+    if (-Not(Test-Path $env:GOROOT))
+    {
+        $file = "$version.windows-$arch.zip"
+        $url = "https://dl.google.com/go/$file"
+        Invoke-WebRequest -UserAgent wget -Uri $url -OutFile $file
+        &7z x -oC:\go $file > $null
+        Move-Item -Path C:\go\go -Destination $env:GOROOT -force
+    }
 }
-Write-Host "Go $version on $arch"
-$env:GOROOT = "C:\go\$version"
-if (-Not(Test-Path $env:GOROOT))
-{
-    $file = "$version.windows-$arch.zip"
-    $url = "https://dl.google.com/go/$file"
-    Invoke-WebRequest -UserAgent wget -Uri $url -OutFile $file
-    &7z x -oC:\go $file > $null
-    Move-Item -Path C:\go\go -Destination $env:GOROOT -force
+
+if ($version -eq "gotip") {
+    Exit 0
+    $latest = Invoke-WebRequest -Uri https://golang.org/VERSION?m=text -UseBasicParsing | Select-Object -ExpandProperty Content
+    GetGo $latest
+    $env:GOROOT_BOOTSTRAP = $env:GOROOT
+    $env:GOROOT = "C:\go\go-tip"
+    Write-Host "Building Go with GOROOT_BOOTSTRAP $env:GOROOT_BOOTSTRAP"
+    if (-Not(Test-Path $env:GOROOT)) {
+        git clone https://go.googlesource.com/go C:\go\go-tip
+        Push-Location -Path C:\go\go-tip\src
+    } else {
+        Push-Location -Path C:\go\go-tip\src
+        git pull
+    }
+    .\make.bat
+    Pop-Location
+} else {
+    # Install Go
+    Write-Host "Finding latest patch version for $version"
+    $version = Invoke-WebRequest -Uri 'https://golang.org/dl/?mode=json&include=all' -UseBasicParsing | foreach {$_.Content} | ConvertFrom-Json | foreach {$_.version} | Select-String -Pattern "^$version($|\.|beta|rc)" | Select-Object -First 1 | foreach {$_.Line}
+    Write-Host "Go $version on $arch"
+    GetGo $version
 }
 
 $env:GOPATH = "C:\gopath"
-$env:PATH += ";C:\procdump;C:\mingw64\bin;$env:GOROOT\bin;$env:GOPATH\bin"
+$env:PATH += ";$env:GOROOT\bin;$env:GOPATH\bin"
 Write-Host $env:PATH
 Write-Host $env:GOROOT
 Write-Host $env:GOPATH
+
 go version
 go env
 mingw32-make test
