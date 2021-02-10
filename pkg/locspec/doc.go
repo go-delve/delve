c068861f95ec570fb7ea93638c87338f25c96af3
Alessandro Arzilli <alessandro.arzilli@gmail.com>
2021-02-10 18:31:37 +0100

    locspec,proc: improve documentation (#2338)
    * Reformat pkg/locspec documentation so that it is formatted correctly
      by godoc/pkgsite
    * Unexport some types and variables in proc that don't need to be
      exported.

--- pkg/locspec/doc.go
+++ pkg/locspec/doc.go
@@ -3,13 +3,13 @@
 //
 // Location spec examples:
 //
-// locStr ::= <filename>:<line> | <function>[:<line>] | /<regex>/ | (+|-)<offset> | <line> | *<address>
-// * <filename> can be the full path of a file or just a suffix
-// * <function> ::= <package>.<receiver type>.<name> | <package>.(*<receiver type>).<name> | <receiver type>.<name> | <package>.<name> | (*<receiver type>).<name> | <name>
-// * <function> must be unambiguous
-// * /<regex>/ will return a location for each function matched by regex
-// * +<offset> returns a location for the line that is <offset> lines after the current line
-// * -<offset> returns a location for the line that is <offset> lines before the current line
-// * <line> returns a location for a line in the current file
-// * *<address> returns the location corresponding to the specified address
+//  locStr ::= <filename>:<line> | <function>[:<line>] | /<regex>/ | (+|-)<offset> | <line> | *<address>
+//  * <filename> can be the full path of a file or just a suffix
+//  * <function> ::= <package>.<receiver type>.<name> | <package>.(*<receiver type>).<name> | <receiver type>.<name> | <package>.<name> | (*<receiver type>).<name> | <name>
+//    <function> must be unambiguous
+//  * /<regex>/ will return a location for each function matched by regex
+//  * +<offset> returns a location for the line that is <offset> lines after the current line
+//  * -<offset> returns a location for the line that is <offset> lines before the current line
+//  * <line> returns a location for a line in the current file
+//  * *<address> returns the location corresponding to the specified address
 package locspec
