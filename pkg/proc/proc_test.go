ea9541b860b9448fa4acea0d7dd89351ae41bc47
Alessandro Arzilli <alessandro.arzilli@gmail.com>
2021-04-21 22:37:53 +0200

    proc: disable part of TestAttachDetach test on macOS (#2429)
    There seems to be a problem where debugserver will leave a zombie
    process instead of detaching correctly, we are sending the right
    commands, it doesn't seem to be a problem with Delve.

--- pkg/proc/proc_test.go
+++ pkg/proc/proc_test.go
@@ -2830,12 +2830,16 @@ func TestAttachDetach(t *testing.T) {
 
 	assertNoError(p.Detach(false), t, "Detach")
 
-	resp, err := http.Get("http://127.0.0.1:9191/nobp")
-	assertNoError(err, t, "Page request after detach")
-	bs, err := ioutil.ReadAll(resp.Body)
-	assertNoError(err, t, "Reading /nobp page")
-	if out := string(bs); !strings.Contains(out, "hello, world!") {
-		t.Fatalf("/nobp page does not contain \"hello, world!\": %q", out)
+	if runtime.GOOS != "darwin" {
+		// Debugserver sometimes will leave a zombie process after detaching, this
+		// seems to be a bug with debugserver.
+		resp, err := http.Get("http://127.0.0.1:9191/nobp")
+		assertNoError(err, t, "Page request after detach")
+		bs, err := ioutil.ReadAll(resp.Body)
+		assertNoError(err, t, "Reading /nobp page")
+		if out := string(bs); !strings.Contains(out, "hello, world!") {
+			t.Fatalf("/nobp page does not contain \"hello, world!\": %q", out)
+		}
 	}
 
 	cmd.Process.Kill()
