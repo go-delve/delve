d2bca7a30775188878770361f6d34574ca2a7560
Alessandro Arzilli <alessandro.arzilli@gmail.com>
2021-05-17 18:56:42 +0200

    pkg/proc/native/linux: fix target crashes induced by RequestManualStop (#2484)
    A RequestManualStop received while the target program is stopped can
    induce a crash when the target is restarted.
    This is caused by the phantom breakpoint detection that was introduced
    in PR #2179 / commit e69d536.
    Instead of always interpreting an unexplained SIGTRAP as a phantom
    breakpoint memorize all possible unreported breakpoint hits and only
    act on it when the thread hasn't moved from one.
    
    Also clarifies the behavior of the halt command when it is received
    while the target is stopped or in the process of stopping.

--- pkg/proc/native/proc_linux.go
+++ pkg/proc/native/proc_linux.go
@@ -576,12 +576,28 @@ func (dbp *nativeProcess) stop(trapthread *nativeThread) (*nativeThread, error) 
 	switchTrapthread := false
 
 	// set breakpoints on SIGTRAP threads
+	var err1 error
 	for _, th := range dbp.threads {
-		if th.CurrentBreakpoint.Breakpoint == nil && th.os.setbp {
-			if err := th.SetCurrentBreakpoint(true); err != nil {
-				return nil, err
+		pc, _ := th.PC()
+
+		if !th.os.setbp && pc != th.os.phantomBreakpointPC {
+			// check if this could be a breakpoint hit anyway that the OS hasn't notified us about, yet.
+			if _, ok := dbp.FindBreakpoint(pc, dbp.BinInfo().Arch.BreakInstrMovesPC()); ok {
+				th.os.phantomBreakpointPC = pc
 			}
 		}
+
+		if pc != th.os.phantomBreakpointPC {
+			th.os.phantomBreakpointPC = 0
+		}
+
+		if th.CurrentBreakpoint.Breakpoint == nil && th.os.setbp {
+			if err := th.SetCurrentBreakpoint(true); err != nil {
+				err1 = err
+				continue
+			}
+		}
+
 		if th.CurrentBreakpoint.Breakpoint == nil && th.os.setbp && (th.Status != nil) && ((*sys.WaitStatus)(th.Status).StopSignal() == sys.SIGTRAP) && dbp.BinInfo().Arch.BreakInstrMovesPC() {
 			manualStop := false
 			if th.ThreadID() == trapthread.ThreadID() {
@@ -589,7 +605,7 @@ func (dbp *nativeProcess) stop(trapthread *nativeThread) (*nativeThread, error) 
 				manualStop = dbp.manualStopRequested
 				dbp.stopMu.Unlock()
 			}
-			if !manualStop {
+			if !manualStop && th.os.phantomBreakpointPC == pc {
 				// Thread received a SIGTRAP but we don't have a breakpoint for it and
 				// it wasn't sent by a manual stop request. It's either a hardcoded
 				// breakpoint or a phantom breakpoint hit (a breakpoint that was hit but
@@ -623,6 +639,9 @@ func (dbp *nativeProcess) stop(trapthread *nativeThread) (*nativeThread, error) 
 				}
 			}
 		}
+	}
+	if err1 != nil {
+		return nil, err1
 	}
 
 	if switchTrapthread {
