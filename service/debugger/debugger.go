8eb01b695174341a5d8288bf6a0549e04099fffd
Derek Parker <parkerderek86@gmail.com>
2021-06-28 15:28:14 -0700

    service/debugger: clarify CreateBreakpoint docs

--- service/debugger/debugger.go
+++ service/debugger/debugger.go
@@ -622,7 +622,15 @@ func (d *Debugger) state(retLoadCfg *proc.LoadConfig) (*api.DebuggerState, error
 
 // CreateBreakpoint creates a breakpoint using information from the provided `requestedBp`.
 // This function accepts several different ways of specifying where and how to create the
-// breakpoint that has been requested.
+// breakpoint that has been requested. Any error encountered during the attempt to set the
+// breakpoint will be returned to the caller.
+//
+// The ways of specifying a breakpoint are listed below in the order they are considered by
+// this function:
+//
+// - If requestedBp.TraceReturn is true then it is expected that
+// requestedBp.Addrs will contain the list of return addresses
+// supplied by the caller.
 //
 // - If requestedBp.File is not an empty string the breakpoint
 // will be created on the specified file:line location
@@ -634,11 +642,11 @@ func (d *Debugger) state(retLoadCfg *proc.LoadConfig) (*api.DebuggerState, error
 // - If requestedBp.Addrs is filled it will create a logical breakpoint
 // corresponding to all specified addresses.
 //
-// - If requestedBp.TraceReturn is true then it is expected that
-// requestedBp.Addrs will contain the list of return addresses
-// supplied by the caller.
-//
 // - Otherwise the value specified by arg.Breakpoint.Addr will be used.
+//
+// Note that this method will use the first successful method in order to
+// create a breakpoint, so mixing different fields will not result is multiple
+// breakpoints being set.
 func (d *Debugger) CreateBreakpoint(requestedBp *api.Breakpoint) (*api.Breakpoint, error) {
 	d.targetMutex.Lock()
 	defer d.targetMutex.Unlock()
