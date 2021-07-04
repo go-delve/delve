399549db58bc139e13e985d4a7bf6f2aa3f3b370
aarzilli <alessandro.arzilli@gmail.com>
2021-07-04 11:29:12 +0200

    proc/core/minidump,proc/gdbserial: update broken ext links
    The LLVM project archived their repository and moved it elsewhere,
    Microsoft broke their minidump documentation URLs.

--- pkg/proc/gdbserial/gdbserver.go
+++ pkg/proc/gdbserial/gdbserver.go
@@ -11,7 +11,7 @@
 // The protocol is specified at:
 //   https://sourceware.org/gdb/onlinedocs/gdb/Remote-Protocol.html
 // with additional documentation for lldb specific extensions described at:
-//   https://github.com/llvm-mirror/lldb/blob/master/docs/lldb-gdb-remote.txt
+//   https://github.com/llvm/llvm-project/blob/main/lldb/docs/lldb-gdb-remote.txt
 //
 // Terminology:
 //  * inferior: the program we are trying to debug
