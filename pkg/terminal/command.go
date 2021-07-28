56731bd88a880ae8563ff23498793e6b3bd6641e
Derek Parker <parkerderek86@gmail.com>
2021-07-28 04:12:51 -0700

    Documentation: Improve help output for examinemem (#2623)
    Before this change when you typed `help` at the Delve prompt you would
    only see the following:
    
    ```
    examinemem (alias: x)  Examine memory:
    ```
    
    Now with this patch the output is more descriptive:
    
    ```
    examinemem (alias: x)  Examine raw memory at the given address.
    ```

--- pkg/terminal/command.go
+++ pkg/terminal/command.go
@@ -487,7 +487,9 @@ Defines <alias> as an alias to <command> or removes an alias.`},
 If locspec is omitted edit will open the current source file in the editor, otherwise it will open the specified location.`},
 		{aliases: []string{"libraries"}, cmdFn: libraries, helpMsg: `List loaded dynamic libraries`},
 
-		{aliases: []string{"examinemem", "x"}, group: dataCmds, cmdFn: examineMemoryCmd, helpMsg: `Examine memory:
+		{aliases: []string{"examinemem", "x"}, group: dataCmds, cmdFn: examineMemoryCmd, helpMsg: `Examine raw memory at the given address.
+
+Examine memory:
 
 	examinemem [-fmt <format>] [-count|-len <count>] [-size <size>] <address>
 	examinemem [-fmt <format>] [-count|-len <count>] [-size <size>] -x <expression>
