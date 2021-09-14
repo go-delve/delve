58351ca4b67a0acf2cb092e895069b67114eae2f
Polina Sokolova <polinasok@users.noreply.github.com>
2021-09-14 12:00:03 -0700

    Refine message wording, log Go version

--- pkg/goversion/compat.go
+++ pkg/goversion/compat.go
@@ -10,10 +10,10 @@ var (
 	MinSupportedVersionOfGoMinor = 15
 	MaxSupportedVersionOfGoMajor = 1
 	MaxSupportedVersionOfGoMinor = 17
-	goTooOldErr                  = fmt.Errorf("Version of Go may be too old to work with this version of Delve (minimum supported version %d.%d, suppress this error with --check-go-version=false)", MinSupportedVersionOfGoMajor, MinSupportedVersionOfGoMinor)
-	goTooOldWarn                 = fmt.Errorf("Warning: version of Go may be too old to work with this version of Delve (minimum supported version %d.%d)", MinSupportedVersionOfGoMajor, MinSupportedVersionOfGoMinor)
-	dlvTooOldErr                 = fmt.Errorf("Version of Delve is too old for this version of Go (maximum supported version %d.%d, suppress this error with --check-go-version=false)", MaxSupportedVersionOfGoMajor, MaxSupportedVersionOfGoMinor)
-	dlvTooOldWarn                = fmt.Errorf("Warning: version of Delve is too old for this version of Go (maximum supported version %d.%d)", MaxSupportedVersionOfGoMajor, MaxSupportedVersionOfGoMinor)
+	goTooOldErr                  = fmt.Sprintf("Go version %%s is too old for this version of Delve (minimum supported version %d.%d, suppress this error with --check-go-version=false)", MinSupportedVersionOfGoMajor, MinSupportedVersionOfGoMinor)
+	goTooOldWarn                 = fmt.Sprintf("WARNING: undefined behavior - Go version %%s is too old for this version of Delve (minimum supported version %d.%d)", MinSupportedVersionOfGoMajor, MinSupportedVersionOfGoMinor)
+	dlvTooOldErr                 = fmt.Sprintf("Version of Delve is too old for Go version %%s (maximum supported version %d.%d, suppress this error with --check-go-version=false)", MaxSupportedVersionOfGoMajor, MaxSupportedVersionOfGoMinor)
+	dlvTooOldWarn                = fmt.Sprintf("WARNING: undefined behavior - version of Delve is too old for Go version %%s (maximum supported version %d.%d)", MaxSupportedVersionOfGoMajor, MaxSupportedVersionOfGoMinor)
 )
 
 // Compatible checks that the version specified in the producer string is compatible with
@@ -23,19 +23,20 @@ func Compatible(producer string, warnonly bool) error {
 	if ver.IsDevel() {
 		return nil
 	}
+	verstr := fmt.Sprintf("%d.%d.%d", ver.Major, ver.Minor, ver.Rev)
 	if !ver.AfterOrEqual(GoVersion{MinSupportedVersionOfGoMajor, MinSupportedVersionOfGoMinor, -1, 0, 0, ""}) {
 		if warnonly {
-			fmt.Fprintf(os.Stderr, "%v\n", goTooOldWarn)
+			fmt.Fprintf(os.Stderr, "%v\n", fmt.Sprintf(goTooOldWarn, verstr))
 			return nil
 		}
-		return goTooOldErr
+		return fmt.Errorf(goTooOldErr, verstr)
 	}
 	if ver.AfterOrEqual(GoVersion{MaxSupportedVersionOfGoMajor, MaxSupportedVersionOfGoMinor + 1, -1, 0, 0, ""}) {
 		if warnonly {
-			fmt.Fprintf(os.Stderr, "%v\n", dlvTooOldWarn)
+			fmt.Fprintf(os.Stderr, "%v\n", fmt.Sprintf(dlvTooOldWarn, verstr))
 			return nil
 		}
-		return dlvTooOldErr
+		return fmt.Errorf(dlvTooOldErr, verstr)
 	}
 	return nil
 }
