d85c0ef33da232ade5d969e935b02d394592f09e
aarzilli <alessandro.arzilli@gmail.com>
2021-11-02 08:54:41 +0100

    version: fix build version using buildInfo
    In go1.18 buildInfo will include the git revision hash, use that to fix
    the Build field of Version so that it is correct even if Delve wasn't
    built using make.go.

---
+++ pkg/version/fixbuild.go
@@ -0,0 +1,23 @@
+//go:build go1.18
+// +build go1.18
+
+package version
+
+import "runtime/debug"
+
+func init() {
+	fixBuild = buildInfoFixBuild
+}
+
+func buildInfoFixBuild(v *Version) {
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+	for i := range info.Settings {
+		if info.Settings[i].Key == "gitrevision" {
+			v.Build = info.Settings[i].Value
+			break
+		}
+	}
+}
