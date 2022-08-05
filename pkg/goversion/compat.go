65d5eb73803a288a056ea2eab7b17a3f7c7301f2
Alessandro Arzilli <alessandro.arzilli@gmail.com>
2022-08-05 21:04:26 +0200

    TeamCity,goversion: bump test matrix (#3092)
    Add Go 1.19 to test matrix, drop 1.16.

--- pkg/goversion/compat.go
+++ pkg/goversion/compat.go
@@ -8,7 +8,7 @@ import (
 
 var (
 	MinSupportedVersionOfGoMajor = 1
-	MinSupportedVersionOfGoMinor = 16
+	MinSupportedVersionOfGoMinor = 17
 	MaxSupportedVersionOfGoMajor = 1
 	MaxSupportedVersionOfGoMinor = 19
 	goTooOldErr                  = fmt.Sprintf("Go version %%s is too old for this version of Delve (minimum supported version %d.%d, suppress this error with --check-go-version=false)", MinSupportedVersionOfGoMajor, MinSupportedVersionOfGoMinor)
