8e126a089cbb7e986480dbffa666895fec8710ef
Derek Parker <parkerderek86@gmail.com>
2023-02-01 18:08:43 -0800

    cmd/dlv: fix exit status for trace subcommand
    We currently return an error when the command exits because a process
    exited error is always returned. Detect this like we do in other code
    paths to return a 0 exit code instead of nonzero which indicates the
    command itself failed and can confuse other tools.

--- cmd/dlv/cmds/commands.go
+++ cmd/dlv/cmds/commands.go
@@ -732,6 +732,9 @@ func traceCmd(cmd *cobra.Command, args []string) {
 		err = cmds.Call("continue", t)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
+			if strings.Contains(err.Error(), "exited") {
+				return 0
+			}
 			return 1
 		}
 		return 0
