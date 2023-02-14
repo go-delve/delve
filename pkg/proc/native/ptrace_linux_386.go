95e33edf530af5bbd0fe7bf554bf0edb6f88240a
Alessandro Arzilli <alessandro.arzilli@gmail.com>
2023-02-14 18:32:13 +0100

    proc/native: fix syscalls to SYS_PROCESS_VM_READV (and WRITEV) (#3273)
    Per https://pkg.go.dev/unsafe#Pointer conversions from unsafe.Pointer
    to uintptr are only safe in limited circumstances. In particular only
    conversions made in the syscall call are pinned.
    Additionally add a call to runtime.KeepAlive to mitigate the bug
    described in: https://github.com/golang/go/issues/58351

--- pkg/proc/native/ptrace_linux_386.go
+++ pkg/proc/native/ptrace_linux_386.go
@@ -69,9 +69,7 @@ func processVmRead(tid int, addr uintptr, data []byte) (int, error) {
 	len_iov := uint32(len(data))
 	local_iov := sys.Iovec{Base: &data[0], Len: len_iov}
 	remote_iov := remoteIovec{base: addr, len: uintptr(len_iov)}
-	p_local := uintptr(unsafe.Pointer(&local_iov))
-	p_remote := uintptr(unsafe.Pointer(&remote_iov))
-	n, _, err := syscall.Syscall6(sys.SYS_PROCESS_VM_READV, uintptr(tid), p_local, 1, p_remote, 1, 0)
+	n, _, err := syscall.Syscall6(sys.SYS_PROCESS_VM_READV, uintptr(tid), uintptr(unsafe.Pointer(&local_iov)), 1, uintptr(unsafe.Pointer(&remote_iov)), 1, 0)
 	if err != syscall.Errno(0) {
 		return 0, err
 	}
@@ -83,9 +81,7 @@ func processVmWrite(tid int, addr uintptr, data []byte) (int, error) {
 	len_iov := uint32(len(data))
 	local_iov := sys.Iovec{Base: &data[0], Len: len_iov}
 	remote_iov := remoteIovec{base: addr, len: uintptr(len_iov)}
-	p_local := uintptr(unsafe.Pointer(&local_iov))
-	p_remote := uintptr(unsafe.Pointer(&remote_iov))
-	n, _, err := syscall.Syscall6(sys.SYS_PROCESS_VM_WRITEV, uintptr(tid), p_local, 1, p_remote, 1, 0)
+	n, _, err := syscall.Syscall6(sys.SYS_PROCESS_VM_WRITEV, uintptr(tid), uintptr(unsafe.Pointer(&local_iov)), 1, uintptr(unsafe.Pointer(&remote_iov)), 1, 0)
 	if err != syscall.Errno(0) {
 		return 0, err
 	}
