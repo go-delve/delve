go-delve/delve

commit cc05aa20641b347d99b09299567e0e0a61881c04

TeamCity: actually run linux/386 tests on linux/386 (#3392)

.teamcity/settings.kts
@@ -192,6 +192,7 @@ class TestBuild(val os: String, val arch: String, version: String, buildId: Abso
                         --env TEAMCITY_VERSION=${'$'}TEAMCITY_VERSION
                         --env CI=true
                         --privileged
+                        --platform linux/$dockerArch
                         $dockerArch/ubuntu:20.04
                         /delve/_scripts/test_linux.sh ${"go$version"} $arch
                     """.trimIndent()

commit b62cf239a818552b3e680853cb5a2a04f0456647

TeamCity: fix linux/arm64 build
Change cc05aa2 broke linux/arm64 builds, this should fix them.

.teamcity/settings.kts
@@ -176,6 +176,12 @@ class TestBuild(val os: String, val arch: String, version: String, buildId: Abso
                         arch
                     }
                 }
+                val dockerPlatformArch = when (arch) {
+                    "arm64" -> "arm64/v8"
+                    else -> {
+                        dockerArch
+                    }
+                }
                 dockerCommand {
                     name = "Pull Ubuntu"
                     commandType = other {