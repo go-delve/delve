656c4f13df68bae53c3ec1aba71f4a90860b52ed
Derek Parker <parkerderek86@gmail.com>
2023-06-16 00:38:19 -0700

    pkg/proc: improve stripped binary support with PIE (#3421)
    Take into account static base for PIE binaries.

--- pkg/proc/pclntab.go
+++ pkg/proc/pclntab.go
@@ -1,39 +1,10 @@
 package proc
 
 import (
-	"bytes"
-	"debug/buildinfo"
 	"debug/elf"
 	"debug/gosym"
-	"encoding/binary"
 	"fmt"
-	"strings"
 )
-
-// From go/src/debug/gosym/pclntab.go
-const (
-	go12magic  = 0xfffffffb
-	go116magic = 0xfffffffa
-	go118magic = 0xfffffff0
-	go120magic = 0xfffffff1
-)
-
-// Select the magic number based on the Go version
-func magicNumber(goVersion string) []byte {
-	bs := make([]byte, 4)
-	var magic uint32
-	if strings.Compare(goVersion, "go1.20") >= 0 {
-		magic = go120magic
-	} else if strings.Compare(goVersion, "go1.18") >= 0 {
-		magic = go118magic
-	} else if strings.Compare(goVersion, "go1.16") >= 0 {
-		magic = go116magic
-	} else {
-		magic = go12magic
-	}
-	binary.LittleEndian.PutUint32(bs, magic)
-	return bs
-}
 
 func readPcLnTableElf(exe *elf.File, path string) (*gosym.Table, error) {
 	// Default section label is .gopclntab
@@ -42,7 +13,7 @@ func readPcLnTableElf(exe *elf.File, path string) (*gosym.Table, error) {
 	section := exe.Section(sectionLabel)
 	if section == nil {
 		// binary may be built with -pie
-		sectionLabel = ".data.rel.ro"
+		sectionLabel = ".data.rel.ro.gopclntab"
 		section = exe.Section(sectionLabel)
 		if section == nil {
 			return nil, fmt.Errorf("could not read section .gopclntab")
@@ -53,17 +24,6 @@ func readPcLnTableElf(exe *elf.File, path string) (*gosym.Table, error) {
 		return nil, fmt.Errorf("found section but could not read .gopclntab")
 	}
 
-	bi, err := buildinfo.ReadFile(path)
-	if err != nil {
-		return nil, err
-	}
-
-	// Find .gopclntab by magic number even if there is no section label
-	magic := magicNumber(bi.GoVersion)
-	pclntabIndex := bytes.Index(tableData, magic)
-	if pclntabIndex >= 0 {
-		tableData = tableData[pclntabIndex:]
-	}
 	addr := exe.Section(".text").Addr
 	lineTable := gosym.NewLineTable(tableData, addr)
 	symTable, err := gosym.NewTable([]byte{}, lineTable)
