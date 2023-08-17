993e46d97b389c865b4a7a111f32a8e1caa1c20f
Derek Parker <parkerderek86@gmail.com>
2023-08-17 15:03:01 -0700

    pkg/proc: Fix handling of uint type in ebpf backend

--- _fixtures/ebpf_trace3.go
+++ _fixtures/ebpf_trace3.go
@@ -6,17 +6,18 @@ import (
 )
 
 func main() {
+	str := "abcdefghijklmnopqrstuvqxyz"
 	i := int64(0)
 	for i = 0; i < 5; i++ {
-		tracedFunction(i, i%5 == 0)
+		tracedFunction(i, i%5 == 0, str[i])
 	}
 	for i = 5; i < 10; i++ {
-		tracedFunction(i, i%5 == 0)
-		time.Sleep(time.Second)
+		tracedFunction(i, i%5 == 0, str[i])
+		time.Sleep(time.Millisecond)
 	}
 }
 
 //go:noinline
-func tracedFunction(x int64, b bool) {
-	fmt.Println(x, b)
+func tracedFunction(x int64, b bool, r byte) {
+	fmt.Println(x, b, r)
 }
