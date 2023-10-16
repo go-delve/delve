04bb7fda337db13c7cc9fde0c607ea25085e6a1e
Joel Sing <joel@sing.id.au>
2023-10-17 02:56:17 +1100

    proc/native: use cgo instead of C for freebsd (#3529)
    There is no benefit to having a small part of the freebsd backend
    implemented in C, rather than using cgo. While here, add a missing
    call to procstat_close and check the return value of
    procstat_getpathname.

--- pkg/proc/native/proc_freebsd.h
+++
@@ -1,9 +0,0 @@
-#include <sys/types.h>
-#include <sys/user.h>
-#include <libutil.h>
-#include <libprocstat.h>
-
-char * find_command_name(int pid);
-char * find_executable(int pid);
-int find_status(int pid);
-uintptr_t get_entry_point(int pid);
