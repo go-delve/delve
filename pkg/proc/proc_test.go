1c5c0a54b6e69c52604caee0e8b3e881458fec21
Derek Parker <parkerderek86@gmail.com>
2023-11-01 18:28:09 -0700

    Improve test skip conditions for TestDebugStripped2

--- pkg/proc/proc_test.go
+++ pkg/proc/proc_test.go
@@ -3234,12 +3234,14 @@ func TestDebugStripped(t *testing.T) {
 
 func TestDebugStripped2(t *testing.T) {
 	// Currently only implemented for Linux ELF executables.
-	// TODO(derekparker): Add support for PE.
-	skipOn(t, "broken on darwin/amd64/pie", "darwin")
-	skipOn(t, "not working on windows", "windows")
-	skipOn(t, "not working on freebsd", "freebsd")
-	skipOn(t, "not working on linux/386 with PIE", "linux", "386", "pie")
-	skipOn(t, "not working on linux/ppc64le when -gcflags=-N -l is passed", "linux", "ppc64le")
+	// TODO(derekparker): Add support for Mach-O and PE.
+	skipUnlessOn(t, "linux/amd64 only", "linux", "amd64")
+	ver, _ := goversion.Parse(runtime.Version())
+	if ver.Major > 0 && !ver.AfterOrEqual(goversion.GoVersion{Major: 1, Minor: 20, Rev: -1}) {
+	}
+	if !goversion.VersionAfterOrEqual(runtime.Version(), 1, 20) {
+		t.Skip("temporarily disabled on Go versions < 1.20")
+	}
 	withTestProcessArgs("inlinestripped", t, "", []string{}, protest.EnableInlining|protest.LinkStrip, func(p *proc.Target, grp *proc.TargetGroup, f protest.Fixture) {
 		setFunctionBreakpointAll(p, t, "fmt.Println")
 
