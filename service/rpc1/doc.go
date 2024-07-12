f85082d2168449d17662b235bf00812aaeda78b2
Oleksandr Redko <oleksandr.red+github@gmail.com>
2024-07-12 18:18:36 +0300

    service/rpc1: add Go Reference doc

---
+++ service/rpc1/doc.go
@@ -0,0 +1,5 @@
+// Package rpc1 implements version 1 of Delve's API and is only
+// kept for backwards compatibility.
+// client.go is the old client code used by Delve's frontend (delve/cmd/dlv) and
+// is preserved for backwards compatibility with integration tests.
+package rpc1
