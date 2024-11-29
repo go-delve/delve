42b533158e4f0f58ed7eec436085f5fe619a4825
Huang Qiqi <huangqiqi@loongson.cn>
2024-11-29 14:51:27 +0800

    delve: support linux-loong64 native debug
    LoongArch is a new RISC ISA, which is independently designed by Loongson Technology.
    
    LoongArch includes a reduced 32-bit version (LA32R), a standard 32-bit version (LA32S)
    and a 64-bit version (LA64), and loong64 is the 64-bit version of LoongArch.
    
    LoongArch documentation: https://github.com/loongson/LoongArch-Documentation.git

--- service/debugger/debugger_test.go
+++ service/debugger/debugger_test.go
@@ -63,6 +63,9 @@ func TestDebugger_LaunchInvalidFormat(t *testing.T) {
 	if runtime.GOARCH == "riscv64" && runtime.GOOS == "linux" {
 		t.Setenv("GOARCH", "amd64")
 	}
+	if runtime.GOARCH == "loong64" && runtime.GOOS == "linux" {
+		t.Setenv("GOARCH", "amd64")
+	}
 	t.Setenv("GOOS", switchOS[runtime.GOOS])
 	exepath := filepath.Join(buildtestdir, debugname)
 	if err := gobuild.GoBuild(debugname, []string{buildtestdir}, fmt.Sprintf("-o %s", exepath)); err != nil {
