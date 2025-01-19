a790ddf42e3e7b82d65de7039f1248ce8a79ce2c
aarzilli <alessandro.arzilli@gmail.com>
2025-01-19 20:15:17 +0100

    proc/debuginfod: add timeouts to debuginfod-find
    Calls to debuginfod-find can take an arbitrarily long time to complete.
    Set timeouts for them unless they were explicitly set by user, also
    redirect its stderr to our stderr so that, if they produce progress
    output, it is seen.
    
    Updates #3906

--- pkg/proc/debuginfod/debuginfod.go
+++ pkg/proc/debuginfod/debuginfod.go
@@ -1,17 +1,26 @@
 package debuginfod
 
 import (
+	"os"
 	"os/exec"
 	"strings"
 )
 
-const debuginfodFind = "debuginfod-find"
+const (
+	debuginfodFind       = "debuginfod-find"
+	debuginfodMaxtimeEnv = "DEBUGINFOD_MAXTIME"
+	debuginfodTimeoutEnv = "DEBUGINFOD_TIMEOUT"
+)
 
 func execFind(args ...string) (string, error) {
 	if _, err := exec.LookPath(debuginfodFind); err != nil {
 		return "", err
 	}
 	cmd := exec.Command(debuginfodFind, args...)
+	if os.Getenv(debuginfodMaxtimeEnv) == "" || os.Getenv(debuginfodTimeoutEnv) == "" {
+		cmd.Env = append(os.Environ(), debuginfodMaxtimeEnv+"=1", debuginfodTimeoutEnv+"=1")
+	}
+	cmd.Stderr = os.Stderr
 	out, err := cmd.Output() // ignore stderr
 	if err != nil {
 		return "", err
