8a2e81974d8c2eaab78f023962cac18c66e3c225
Archana Ravindar <aravinda@redhat.com>
2025-04-08 14:44:02 +0530

    Added test programs to test tracing of defer functions

---
+++ _fixtures/nestdefer.go
@@ -0,0 +1,23 @@
+package main
+
+
+var varc int 
+func C()  {
+	defer D()
+varc += 10*10
+}
+func A() {
+	defer C()
+	varc = 40
+}
+
+func D() {
+     B()
+}
+
+func B() {
+	varc -= 20
+}
+func main() {
+	A()
+}
