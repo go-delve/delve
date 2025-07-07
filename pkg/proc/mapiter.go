6c65eca8cdc7d238464a015de6a5186e3878e73f
冶英杰 <yeyingjie.bluarry@bytedance.com>
2025-07-07 15:54:33 +0800

    panic fix

--- pkg/proc/mapiter.go
+++ pkg/proc/mapiter.go
@@ -234,7 +234,7 @@ func (it *mapIteratorClassic) nextBucket() bool {
 		return false
 	}
 
-	if it.overflow.Kind != reflect.Struct {
+	if it.overflow != nil && it.overflow.Kind != reflect.Struct {
 		it.v.Unreadable = errMapBucketsNotStruct
 		return false
 	}
