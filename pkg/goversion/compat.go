08a9f33c505b9c2fab9859b617f22dd7bc83b058
Derek Parker <parkerderek86@gmail.com>
2025-08-27 13:24:05 -0700

    pkg/goversion: update MinSupportedVersionOfGoMinor
    The oldest version of Go we run in CI is 1.23,
    MinSupportedVersionOfGoMinor should reflect that.

--- pkg/goversion/compat.go
+++ pkg/goversion/compat.go
@@ -12,7 +12,7 @@ import (
 
 var (
 	MinSupportedVersionOfGoMajor = 1
-	MinSupportedVersionOfGoMinor = 22
+	MinSupportedVersionOfGoMinor = 23
 	MaxSupportedVersionOfGoMajor = 1
 	MaxSupportedVersionOfGoMinor = 25
 	goTooOldErr                  = fmt.Sprintf("Go version %%s is too old for this version of Delve (minimum supported version %d.%d, suppress this error with --check-go-version=false)", MinSupportedVersionOfGoMajor, MinSupportedVersionOfGoMinor)
