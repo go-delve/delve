3cc4b2bc1ed83c89166cef36c86bf8e064a4c8fb
Derek Parker <parkerderek86@gmail.com>
2025-09-10 09:04:15 -0700

    address review feedback

--- _fixtures/testnextnethttp.go
+++ _fixtures/testnextnethttp.go
@@ -5,9 +5,9 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"path/filepath"
 	"runtime"
 )
-
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 		runtime.Breakpoint()
@@ -26,15 +26,16 @@ func main() {
 	}
 	port := listener.Addr().(*net.TCPAddr).Port
 	fmt.Printf("LISTENING:%d\n", port)
-	
+
 	// Also write port to a file for tests that can't capture stdout
 	// Include PID in filename to avoid conflicts when tests run in parallel
-	portFile := fmt.Sprintf("/tmp/testnextnethttp_port_%d", os.Getpid())
+	tmpdir := os.TempDir()
+	portFile := filepath.Join(tmpdir, fmt.Sprintf("testnextnethttp_port_%d", os.Getpid()))
 	os.WriteFile(portFile, []byte(fmt.Sprintf("%d", port)), 0644)
-	
+
 	// Clean up port file when program exits
 	defer os.Remove(portFile)
-	
+
 	err = http.Serve(listener, nil)
 	if err != nil {
 		panic(err)
