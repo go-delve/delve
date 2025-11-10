760bc07e3f9de5c502f4ad818d5a67687ab70337
aarzilli <alessandro.arzilli@gmail.com>
2025-11-10 15:15:34 +0100

    *: fix tests
    Fix test build error caused by merge conflict and regenerate capslock
    output.

---
+++ _scripts/capslock.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cl() {
+	echo $1/$2
+	capslock -goos $1 -goarch $2 -packages ./cmd/dlv > _scripts/capslock_$1_$2-output.txt
+}
+cl darwin amd64
+cl darwin arm64
+cl linux 386
+cl linux amd64
+cl linux arm64
+cl linux ppc64le
+cl windows amd64
+cl windows arm64
